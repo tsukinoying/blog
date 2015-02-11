d54137709d3dfc75668211b64f71a4e7e16df003
wangsheng <wangsheng@ihuilian.com>
2015-02-11 11:49:41 +0800

    更新数据库配置

--- config/database.php
+++ config/database.php
@@ -54,13 +54,13 @@ return [
 
 		'mysql' => [
 			'driver'    => 'mysql',
-			'host'      => env('DB_HOST', 'localhost'),
-			'database'  => env('DB_DATABASE', 'forge'),
-			'username'  => env('DB_USERNAME', 'forge'),
+			'host'      => env('DB_HOST', 'localhost:3306'),
+			'database'  => 'blog',
+			'username'  => 'root',
 			'password'  => env('DB_PASSWORD', ''),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
-			'prefix'    => '',
+			'prefix'    => 'blog_',
 			'strict'    => false,
 		],
 
